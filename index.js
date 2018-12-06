4ffc78465f127f6c4e2002ebd888358de9b6c6cb
darryl-snow <dazsnow@gmail.com>
2018-12-07 01:56:16 +0800

    client side MVP - checkout and redirect to complete

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ import path from 'path';
 const port = process.env.PORT || 8000;
 
 const app = express()
-  .use('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/checkout.html'));
-  })
   .use('/complete', (req, res) => {
-    console.log(req);
-    res.sendFile(path.join(__dirname + '/complete.html'));
+    res.sendFile(path.join(__dirname, 'complete.html'));
+  })
+  .use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'checkout.html'));
   })
   .listen(port, (err) => {
     console.log(`Server running on ${port}`);
