c2499dbe866b04ec79a346312e31c916c8391f43
darryl-snow <dazsnow@gmail.com>
2018-12-07 00:37:19 +0800

    use PORT env variable for heroku

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import path from 'path';
 
+const port = process.env.PORT || 8000;
+
 const app = express()
   .use('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/checkout.html'));
   })
-  .listen(8000, (err) => {
-    console.log('Server running');
+  .listen(port, (err) => {
+    console.log(`Server running on ${port}`);
   });
