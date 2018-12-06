868b8a0541e7009d955ed0db867580c81bc18fb5
darryl-snow <dazsnow@gmail.com>
2018-12-07 01:08:46 +0800

    change button style

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express()
   .use('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/checkout.html'));
   })
+  .use('/complete', (req, res) => {
+    console.log(req);
+    res.sendFile(path.join(__dirname + '/complete.html'));
+  })
   .listen(port, (err) => {
     console.log(`Server running on ${port}`);
   });
