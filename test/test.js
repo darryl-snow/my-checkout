53128f9c7302c440ae40bf065cb0dbd32dcb9a94
darryl-snow <dazsnow@gmail.com>
2018-12-06 23:14:59 +0800

    setup

---
+++ test/test.js
@@ -0,0 +1,9 @@
+import assert from 'assert';
+import 'babel-polyfill';
+import '../index';
+
+describe('My Checkout', () => {
+  it('should pass a dummy test', () => {
+    assert.equal(1, 1);
+  });
+});
